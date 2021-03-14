b9254bbc90077d4782fcf4a5e423a649f3ca9364
fayzur <fayzurrahman991@gmail.com>
2021-03-14 17:00:16 +0600

    my protfolio is done

---
+++ js/main.js
@@ -0,0 +1,92 @@
+$(function () {
+  $("#diagram-id-1").circleDiagram({
+    percent: "90%",
+    size: "150",
+    borderWidth: "10",
+    bgFill: "#666666",
+    frFill: "#cb218d",
+    textSize: "24",
+    textColor: "#cb218d",
+  });
+});
+
+$(function () {
+  $("#diagram-id-2").circleDiagram({
+    percent: "85%",
+    size: "150",
+    borderWidth: "10",
+    bgFill: "#666666",
+    frFill: "#cb218d",
+    textSize: "24",
+    textColor: "#cb218d",
+  });
+});
+
+$(function () {
+  $("#diagram-id-3").circleDiagram({
+    percent: "60%",
+    size: "150",
+    borderWidth: "10",
+    bgFill: "#666666",
+    frFill: "#cb218d",
+    textSize: "24",
+    textColor: "#cb218d",
+  });
+});
+
+$(function () {
+  $("#diagram-id-4").circleDiagram({
+    percent: "75%",
+    size: "150",
+    borderWidth: "10",
+    bgFill: "#666666",
+    frFill: "#cb218d",
+    textSize: "24",
+    textColor: "#cb218d",
+  });
+});
+
+$(function () {
+  $("#diagram-id-5").circleDiagram({
+    percent: "60%",
+    size: "150",
+    borderWidth: "10",
+    bgFill: "#666666",
+    frFill: "#cb218d",
+    textSize: "24",
+    textColor: "#cb218d",
+  });
+});
+$(function () {
+  $("#diagram-id-6").circleDiagram({
+    percent: "85%",
+    size: "150",
+    borderWidth: "10",
+    bgFill: "#666666",
+    frFill: "#cb218d",
+    textSize: "24",
+    textColor: "#cb218d",
+  });
+});
+$(function () {
+  $("#diagram-id-7").circleDiagram({
+    percent: "45%",
+    size: "150",
+    borderWidth: "10",
+    bgFill: "#666666",
+    frFill: "#cb218d",
+    textSize: "24",
+    textColor: "#cb218d",
+  });
+});
+$(function () {
+  $("#diagram-id-8").circleDiagram({
+    percent: "55%",
+    size: "150",
+    borderWidth: "10",
+    bgFill: "#666666",
+    frFill: "#cb218d",
+    textSize: "24",
+    textColor: "#cb218d",
+  });
+});
